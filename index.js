3eb40db03798cff57982250a372cc00529613318
Jose <joseluis0528@gmail.com>
2023-03-24 20:47:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(array) {
+   const win = array.find(record => record.result === "W")
+   if (win) {
+    return win.year
+   } else return undefined
+}
